454816265f3141612a6f881e7a6a30b649dbdb32
Long <nve164@gmail.com>
2022-05-29 21:03:26 +0900

    Update mergetwosortedlists.py

--- mergetwosortedlists.py
+++ mergetwosortedlists.py
@@ -66,4 +66,17 @@ class Solution_better:
             prev.next = list2
             
         return new_head.next
-        
+
+class Solution_shorter:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = curr = ListNode(-1)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
